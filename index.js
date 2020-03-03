5402cad6b2e55abd5fe54552e1e11663a61d917d
liboqiao1234 <13693338025@163.com>
2020-03-03 10:31:23 +0800

    test ajax

---
+++ index.js
@@ -0,0 +1,14 @@
+function getList(){
+  $.ajax({
+    url:"data.json",
+    success:function(res){
+      res_j=JSON.stringify(res);
+      alert("欢迎！今天也要努力哦！"+res_j.user[0]);
+    }
+  })
+};
+
+
+$(document).ready(function(){
+  getList();
+});
